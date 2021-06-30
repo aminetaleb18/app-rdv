00cf50752ff68d09c18ef1d28e2292038f45cb37
aminetaleb18 <aminetaleb18@gmail.com>
2021-06-30 16:13:50 +0200

    deuxième commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import { BrowserRouter as Router, Link, Route , Switch } from 'react-router-dom';
+import Login from './components/Login'
+import Register from './components/Register'
+import Home from './components/Home'
 import NavbarMenu from './components/Navbar/NavbarMenu'
 import { useState} from 'react'
 import Navbar from 'react-bootstrap/Navbar'
@@ -9,27 +13,28 @@ function App() {
   const [isOpen, setIsOpen] = useState(false)
 
 	const openMenu = () => {
-		setIsOpen(true)
+		setIsOpen(v => !v)
 	}
 
-
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar expand="lg" variant="light" bg="dark"
-         isOpen={isOpen} setIsOpen={setIsOpen} />
-        <section className="flex">
-					<button text="OUVRIR MENU" onClick={openMenu}/>
-				</section>
+    <Router>
+      <div className="App">
+        <header>
+          {isOpen && <NavbarMenu setIsOpen={setIsOpen} />}
+          <section className="flex">
+            <button text="OUVRIR MENU" onClick={openMenu}>Menu</button>
+          </section>
 
-
-        
-        
-        
-        
-        
-      </header>
-    </div>
+          
+        </header>
+        <Switch>
+            <Route path='/Home' component={() => <Home/>}/>
+            <Route path='/Connexion' component={() => <Login/>}/>
+            <Route path='/Inscription' component={() => <Register/>}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/NavbarMenu.jsx
+++ src/components/Navbar/NavbarMenu.jsx
@@ -1,41 +1,34 @@
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-import Login from '../Login'
-import Register from '../Register'
-import Home from '../Home'
+
 import React from 'react';
 
 
 
-const Navbar = ({ isOpen, setIsOpen }) => {
+const NavbarMenu = ({ isOpen, setIsOpen }) => {
 	const closeMenu = () => {
 		setIsOpen(false)
 	}
 
 	return (
-      
-<Router>
-		<nav className={isOpen}>
+
+			<nav className="App-menu">
+		
+				<ul className="ul">
+					<li><Link to='/Home'>Home</Link></li>
+				</ul>
+				<ul>
+					<li><Link to='/Inscription'>Inscription</Link></li>
+				</ul>
+				<ul>
+					<li><Link to='/Connexion'>Connexion</Link></li>
+				</ul>
+
+				
+
+
+				{/* <button text="FERMER MENU" className='hide' onClick={closeMenu}/> */}
+			</nav>
 	
-			<ul className="ul">
-                <li><Link to='/Home'>Home</Link></li>
-            </ul>
-            <ul>
-                <li><Link to='/Inscription'>Inscription</Link></li>
-            </ul>
-            <ul>
-                <li><Link to='/Connexion'>Connexion</Link></li>
-            </ul>
-
-            <switch>
-                <Route path='/Home' component={() => <Home/>}/>
-			    <Route path='/Connexion' component={() => <Login/>}/>
-				<Route path='/Inscription' component={() => <Register/>}/>
-            </switch>
-
-
-			<button text="FERMER MENu" className='hide' onClick={closeMenu}/>
-		</nav>
-		</Router>
 	)
 }
 
